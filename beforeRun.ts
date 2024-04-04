12ae7567be630c14040a3a7c6842be5e9745a0b1
Kubik <totorogaming.mc@gmail.com>
2024-04-04 18:10:33 +0200

    Auto updates heads when install

--- beforeRun.ts
+++ beforeRun.ts
@@ -1,13 +1,18 @@
-import { mkdirSync } from 'fs';
+import { existsSync, mkdirSync, rmSync } from 'fs';
 import { CreatorDetailList } from './src/data.ts';
 import { resolve } from 'path';
 import sharp from 'sharp';
+import chalk from 'chalk';
+
+process.stdout.write(`[🗿] Updating heads...`);
 
 async function updateHeads() {
     const headsFolder = resolve(__dirname, 'src', 'public', 'heads');
     mkdirSync(headsFolder, { recursive: true });
     await Promise.all(Object.values(CreatorDetailList).map((v) => {
         return new Promise(async (prResolve) => {
+            let outName = resolve(headsFolder, `${v.minecraft}.png`);
+            if (existsSync(outName)) rmSync(outName);
             let first = await (await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${v.minecraft}`)).json();
             let second = JSON.parse(atob(first.properties.find(v => v.name == "textures").value));
 
@@ -18,8 +23,8 @@ async function updateHeads() {
                 .composite([{
                     input: await sharp(imageurl).extract({ left: 40, top: 8, width: 8, height: 8 }).toBuffer()
                 }])
-                .toFile(resolve(headsFolder, `${v.minecraft}.png`), function (err) {
-                    if (err) console.log(err);
+                .toFile(outName, function (err) {
+                    if (err) console.error(`\nError while getting "${v.minecraft}"'s head`, err, "\n\n");
                     prResolve(true);
                 })
         })
@@ -27,3 +32,5 @@ async function updateHeads() {
 }
 
 await updateHeads();
+let timeTaken = process.hrtime.bigint().toString().slice(0, -4);
+process.stdout.write(`\r${chalk.green(Object.keys(CreatorDetailList).length)} heads updated ${chalk.gray(`[${chalk.bold([timeTaken.slice(0, -2), '.', timeTaken.slice(-2)].join(''))}ms]`)}\n`);
