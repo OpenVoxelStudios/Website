a989498160cc057d84738e38e4db021fccc98f26
Kubik <84036232+KodeurKubik@users.noreply.github.com>
2024-10-21 20:36:28 +0200

    Still a lot WIP

--- src/routes/3d.tsx
+++ src/routes/3d.tsx
@@ -1,47 +1,48 @@
 import { Suspense, useEffect, useState } from 'react'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { Environment, Preload } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Environment, Html, Preload, useProgress } from '@react-three/drei'
 import Header from '@/components/header'
 import { onLoad } from '@/router'
 import { ScrollRestoration } from 'react-router-dom'
 import Animations from '@/components/3d/Animations'
 import { easing } from 'maath'
+import UAParser from 'ua-parser-js';
 
-
-function Rig() {
-  const [mouseIn, setMouseIn] = useState<boolean>(false);
-  const canvas = (document.getElementById('canvas1') as HTMLCanvasElement);
-  const { camera } = useThree();
-  const [rotation, setRotation] = useState<number>(0);
-
-  canvas.onmouseenter = (_e) => setMouseIn(true);
-  canvas.onmouseleave = (_e) => setMouseIn(false);
-  canvas.onmousemove = (e) => {
-    if (!mouseIn) return;
-    let bounds = canvas.getBoundingClientRect();
-
-    let goal = Math.PI / 2500 * (bounds.left - e.clientX);
-    setRotation(goal);
-
-    camera.rotateY(goal - rotation);
-  };
-
-  return <></>;
-}
+import '../components/main.css';
 
 function CameraRig() {
   useFrame((state, delta) => {
-    easing.damp3(state.camera.position, [-1 + (state.pointer.x * state.viewport.width) / 3, (1 + state.pointer.y) / 2, 5.5], 0.5, delta)
+    easing.damp3(state.camera.position, [Math.min(Math.max(-0.5 + (state.pointer.x * state.viewport.width) / 3, -3), 1.5), (1 + state.pointer.y) / 2, 4], 0.5, delta)
     state.camera.lookAt(0, 0, 0)
   });
 
   return <></>;
 }
 
+function Loader() {
+  const { progress } = useProgress()
+  return <Html center>{progress.toFixed(2)}% loaded...</Html>
+}
 
+function Scene() {
+  return (
+    <>
+      <Suspense fallback={<Loader />}>
+        <Animations scale={[1, 1, 1]} position={[-1, -1.5, 0]} />
+        <spotLight position={[-5.5, 10, 0]} rotation={[0, 0, -Math.PI / 2]} angle={Math.PI / 13} penumbra={0.6} decay={0} intensity={Math.PI * 4} />
+        <CameraRig />
+        <Environment preset="night" background={false} />
+        <Preload all />
+      </Suspense>
+    </>
+  )
+};
 
 export default function ThreeDRoute() {
   useEffect(onLoad, []);
+  const [device, _setDevice] = useState<string | undefined>(new UAParser(navigator.userAgent).getDevice().type);
+  const [isDesktop, _setIsDesktop] = useState<boolean>(device === undefined || !['wearable', 'mobile'].includes(device));
+  console.log(isDesktop);
 
   return (
     <>
@@ -49,16 +50,23 @@ export default function ThreeDRoute() {
 
       <div id='content' className='content'>
 
-        <Canvas shadows className='glass' id='canvas1' style={{ height: '300px', width: '800px' }} flat camera={{ fov: 75, position: [0, 0, 0] }}>
-          <Suspense fallback={null}>
-            <Animations scale={[1.5, 1.5, 1.5]} />
+        <div className='arg'>
+          <div className='text'>
+            <h1>Fancy Animations.</h1>
+            <p>We brought <b>amazing animations</b> into vanilla Minecraft using <a href='https://animated-java.dev' target='_blank'>Animated Java</a> and <a href='https://www.blockbench.net' target='_blank'>Blockbench</a>!<br />An <b>exhibition room</b> is in the works! You will be able to see the models in 3D and download some of them!</p>
+          </div>
 
-            {/* <Rig /> */}
-            <CameraRig />
-            <Environment preset="warehouse" background={false} />
-            <Preload all />
-          </Suspense>
-        </Canvas>
+          <div className='notextselection glass img'>
+            {isDesktop &&
+              <Canvas shadows id='canvas1' flat camera={{ fov: 75, position: [0, 0, 1] }} style={{ cursor: 'none' }}>
+                <Scene />
+              </Canvas>
+            }
+            {!isDesktop &&
+              <img src='/banner-team.png' loading='lazy' decoding='async' style={{ filter: 'blur(4px)' }} />
+            }
+          </div>
+        </div>
       </div>
 
       <ScrollRestoration />
