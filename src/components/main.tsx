c72dd3c4d1bef1616b20b00937206e314f6d2b33
Kubik <84036232+KodeurKubik@users.noreply.github.com>
2024-10-29 07:10:24 +0100

    Magic stuff

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -82,7 +82,8 @@ export default function MainPage() {
                     <motion.p
                         initial={{ opacity: 0, y: '20px' }}
                         whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1, delay: 0.2 }}
+                        transition={{ duration: 1.5, delay: 0.1, ease: 'easeIn', type: 'spring', bounce: 0.5 }}
+                        viewport={{ once: true, amount: 0.3 }}
                     >
                         <b>All maps are 100% vanilla</b> and require no mods, thanks to Datapacks and Resource packs!<br />* Some Maps may use Mods to enhance the experience, but are not required!
                     </motion.p>
@@ -95,7 +96,8 @@ export default function MainPage() {
                     <motion.p
                         initial={{ opacity: 0, y: '20px' }}
                         whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1, delay: 0.2 }}
+                        transition={{ duration: 1.5, delay: 0.1, ease: 'easeIn', type: 'spring', bounce: 0.5 }}
+                        viewport={{ once: true, amount: 0.3 }}
                     >
                         We brought <b>amazing animations</b> into vanilla Minecraft using <a href='https://animated-java.dev' target='_blank'>Animated Java</a> and <a href='https://www.blockbench.net' target='_blank'>Blockbench</a>!<br />An <b>exhibition room</b> is in the works! You will be able to see the models in 3D and download some of them!
                     </motion.p>
@@ -120,7 +122,8 @@ export default function MainPage() {
                     <motion.p
                         initial={{ opacity: 0, y: '20px' }}
                         whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1, delay: 0.2 }}
+                        transition={{ duration: 1.5, delay: 0.1, ease: 'easeIn', type: 'spring', bounce: 0.5 }}
+                        viewport={{ once: true, amount: 0.3 }}
                     >
                         Check out our <b>Hall of Fame</b> and see every contest we've taken part in and won!
                     </motion.p>
@@ -136,7 +139,8 @@ export default function MainPage() {
                     <motion.p
                         initial={{ opacity: 0, y: '20px' }}
                         whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 1, delay: 0.2, ease: 'easeIn' }}
+                        transition={{ duration: 1.5, delay: 0.1, ease: 'easeIn', type: 'spring', bounce: 0.5 }}
+                        viewport={{ once: true, amount: 0.3 }}
                     >
                         <b>Play all our games effortlessly in one place!</b><br />The Launcher also comes with optimization Mods to even work on slower computers.
                     </motion.p>
@@ -148,28 +152,55 @@ export default function MainPage() {
             </div>
 
             <div className='team' id='team'>
-                <div className='notextselection glass heads'>
-                    {Object.keys(CreatorDetailList).map((creator, index) =>
-                        <motion.div className={(profile == creator ? 'glass ' : '') + 'head'} key={index} onClick={() => setProfile(creator)}
-                            initial={{ scale: 0.6, opacity: 0.6 }}
-                            whileInView={{ scale: 1, opacity: 1 }}
-                            viewport={{ once: true }}
-                            transition={{ duration: 0.2, delay: 0.1 }}
+                <motion.div
+                    className='notextselection glass heads'
+                    variants={{
+                        visible: {
+                            transition: {
+                                staggerChildren: 0.1,
+                            }
+                        }
+                    }}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.3 }}
+                >
+                    {Object.keys(CreatorDetailList).map((creator, index) => (
+                        <motion.div
+                            className={(profile == creator ? 'glass ' : '') + 'head'}
+                            key={index}
+                            onClick={() => setProfile(creator)}
+                            variants={{
+                                hidden: { scale: 0.4, y: '20px' },
+                                visible: { scale: 1, y: 0, transition: { duration: 0.3 } }
+                            }}
                         >
                             <img src={`/heads/${CreatorDetailList[creator as CreatorList].minecraft}.png`} loading='lazy' decoding='async' />
                         </motion.div>
-                    )}
-                </div>
+                    ))}
+                </motion.div>
 
-                <div className='glass notextselection description'>
+                <motion.div
+                    className='glass notextselection description'
+                    layout
+                    transition={{ ease: "easeIn", duration: 0.2 }}
+                >
                     <div className='notextselection top'>
-                        <img className='notextselection' src={`/heads/${CreatorDetailList[profile as CreatorList].minecraft}.png`} loading='lazy' decoding='async' />
+                        <img
+                            className='notextselection'
+                            src={`/heads/${CreatorDetailList[profile as CreatorList].minecraft}.png`}
+                            loading='lazy'
+                            decoding='async'
+                        />
                         <h1 className='yestextselection'>{profile}</h1>
                     </div>
                     <div className='nottop'>
-                        <Markdown remarkPlugins={[remarkGfm]}>{CreatorDetailList[profile as CreatorList].description || "Bro forgot to write something about himself 💀💀"}</Markdown>
+                        <Markdown remarkPlugins={[remarkGfm]}>
+                            {CreatorDetailList[profile as CreatorList].description || "Bro forgot to write something about himself 💀💀"}
+                        </Markdown>
                     </div>
-                </div>
+                </motion.div>
+
             </div>
         </>
     )
