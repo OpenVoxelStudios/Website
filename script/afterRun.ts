b4f48a2cb8a0216ee9cfe6e47aeca3116991079a
Kubik <84036232+KodeurKubik@users.noreply.github.com>
2024-10-30 21:47:55 +0100

    Sitemap fixes yay

--- script/afterRun.ts
+++ script/afterRun.ts
@@ -13,3 +13,15 @@ Object.keys(links).forEach(link => {
     writeFileSync(resolve(outDir, link + `.php`), readFileSync((resolve(root, 'download.php')), { encoding: 'utf-8' }).replace('{{GAME_URL}}', links[link] as string), { encoding: 'utf-8' })
 });
 console.timeEnd(`Created Redirects`);
+
+// This is really bad code but it works :)
+console.time(`Edit sitemap.xml`);
+const sitemap = resolve(outDir, 'sitemap.xml');
+let splitter = '\r\n        </loc>';
+let result = readFileSync(sitemap, { encoding: 'utf-8' });
+
+result = result.split(splitter).map(a => a + (a.endsWith('/') ? '' : '/')).join(splitter);
+result = result.slice(0, -1);
+
+writeFileSync(sitemap, result, { encoding: 'utf-8' });
+console.timeEnd(`Edit sitemap.xml`);
